6b4fa17589f41f1b411a71dd9d022858323deffe
Liubov555 <101938652+Liubov555@users.noreply.github.com>
2022-09-11 18:48:00 +0000

    Deploying to gh-pages from @ Liubov555/goit-js-hw-09@0cba5d00e57954c71c2f2dbf6ad626367015119d 🚀

---
+++ 01-color-switcher.96550479.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){n=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled",!1)})),e.addEventListener("click",(function(){e.setAttribute("disabled",!0),t.removeAttribute("disabled",!1),clearInterval(n)}));var n=null}();
+//# sourceMappingURL=01-color-switcher.96550479.js.map
